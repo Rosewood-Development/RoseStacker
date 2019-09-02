d62849e07e01e7777ecf0b5bfd907af12d6ba1f0
Esophose <esophose@gmail.com>
2019-09-01 22:10:03 -0600

    Table creation

--- src/main/java/dev/esophose/rosestacker/database/migrations/_1_InitialMigration.java
+++ src/main/java/dev/esophose/rosestacker/database/migrations/_1_InitialMigration.java
@@ -6,6 +6,7 @@ import dev.esophose.rosestacker.database.MySQLConnector;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class _1_InitialMigration extends DataMigration {
 
@@ -17,7 +18,55 @@ public class _1_InitialMigration extends DataMigration {
     public void migrate(DatabaseConnector connector, Connection connection, String tablePrefix) throws SQLException {
         String autoIncrement = connector instanceof MySQLConnector ? " AUTO_INCREMENT" : "";
 
-        // TODO
+        // Create StackedBlock table
+        try (Statement statement = connection.createStatement()) {
+            statement.execute("CREATE TABLE " + tablePrefix + "stacked_block (" +
+                    "id INTEGER PRIMARY KEY" + autoIncrement + ", " +
+                    "stack_size INTEGER, " +
+                    "chunk_x INTEGER, " +
+                    "chunk_z INTEGER, " +
+                    "block_x INTEGER, " +
+                    "block_y INTEGER, " +
+                    "block_z INTEGER" +
+                    ")");
+        }
+
+        // Create StackedEntity table
+        try (Statement statement = connection.createStatement()) {
+            statement.execute("CREATE TABLE " + tablePrefix + "stacked_entity (" +
+                    "id INTEGER PRIMARY KEY" + autoIncrement + ", " +
+                    "stack_size INTEGER, " +
+                    "stack_entities TEXT, " +
+                    "chunk_x INTEGER, " +
+                    "chunk_z INTEGER, " +
+                    "entity_uuid VARCHAR(36), " +
+                    ")");
+        }
+
+        // Create StackedItem table
+        try (Statement statement = connection.createStatement()) {
+            statement.execute("CREATE TABLE " + tablePrefix + "stacked_item (" +
+                    "id INTEGER PRIMARY KEY" + autoIncrement + ", " +
+                    "stack_size INTEGER, " +
+                    "chunk_x INTEGER, " +
+                    "chunk_z INTEGER, " +
+                    "entity_uuid VARCHAR(36), " +
+                    ")");
+        }
+
+        // Create StackedSpawner table
+        try (Statement statement = connection.createStatement()) {
+            statement.execute("CREATE TABLE " + tablePrefix + "stacked_spawner (" +
+                    "id INTEGER PRIMARY KEY" + autoIncrement + ", " +
+                    "stack_size INTEGER, " +
+                    "chunk_x INTEGER, " +
+                    "chunk_z INTEGER, " +
+                    "block_x INTEGER, " +
+                    "block_y INTEGER, " +
+                    "block_z INTEGER" +
+                    ")");
+        }
+
     }
 
 }
--- src/main/java/dev/esophose/rosestacker/manager/StackManager.java
+++ src/main/java/dev/esophose/rosestacker/manager/StackManager.java
@@ -22,11 +22,9 @@ import org.bukkit.scheduler.BukkitTask;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 
