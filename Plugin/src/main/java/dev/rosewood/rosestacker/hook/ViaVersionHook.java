f60958eafdae6fa707efc6d19114afe59c72afcf
Esophose <esophose@gmail.com>
2021-05-06 14:16:13 -0600

    Fix ViaVersion hook not working for dev builds due to a package change

--- Plugin/src/main/java/dev/rosewood/rosestacker/hook/ViaVersionHook.java
+++ Plugin/src/main/java/dev/rosewood/rosestacker/hook/ViaVersionHook.java
@@ -1,16 +1,34 @@
 package dev.rosewood.rosestacker.hook;
 
-import us.myles.ViaVersion.ViaVersionPlugin;
-import us.myles.ViaVersion.api.configuration.ConfigurationProvider;
-
 public class ViaVersionHook {
 
     public static void suppressMetadataErrors() {
-        ConfigurationProvider config = ViaVersionPlugin.getInstance().getConfigurationProvider();
-        // If we don't set this, the console will get spammed due to the dynamic tags
-        config.set("suppress-metadata-errors", true);
-        config.saveConfig();
-        config.reloadConfig();
+        // ViaVersion has changed its package name in the newer dev builds
+        boolean oldPackage;
+        try {
+            Class.forName("us.myles.ViaVersion.ViaVersionPlugin");
+            oldPackage = true;
+        } catch (ClassNotFoundException e) {
+            try {
+                Class.forName("com.viaversion.viaversion.ViaVersionPlugin");
+                oldPackage = false;
+            } catch (ClassNotFoundException e2) {
+                return;
+            }
+        }
+
+        // Try to suppress metadata errors so the dynamic tags don't spam the console
+        if (oldPackage) {
+            us.myles.ViaVersion.api.configuration.ConfigurationProvider config = us.myles.ViaVersion.ViaVersionPlugin.getInstance().getConfigurationProvider();
+            config.set("suppress-metadata-errors", true);
+            config.saveConfig();
+            config.reloadConfig();
+        } else {
+            com.viaversion.viaversion.api.configuration.ConfigurationProvider config = com.viaversion.viaversion.ViaVersionPlugin.getInstance().getConfigurationProvider();
+            config.set("suppress-metadata-errors", true);
+            config.saveConfig();
+            config.reloadConfig();
+        }
     }
 
 }
