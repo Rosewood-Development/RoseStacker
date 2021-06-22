d9569ba72b01d44d0bdba8052a7c5029b401beee
Esophose <esophose@gmail.com>
2021-06-22 16:37:52 -0600

    Use the smallest query between the X and Z chunk coordinates

--- Plugin/src/main/java/dev/rosewood/rosestacker/utils/QueryUtils.java
+++ Plugin/src/main/java/dev/rosewood/rosestacker/utils/QueryUtils.java
@@ -10,24 +10,51 @@ import org.bukkit.Chunk;
 public final class QueryUtils {
 
     public static String buildChunksWhere(Collection<Chunk> chunks) {
-        Map<String, Map<Integer, TreeSet<Integer>>> queryValues = new HashMap<>(); // Map<WorldName, Map<ChunkX, Set<ChunkZ>>>
+        // Map<WorldName, Map<ChunkX, Set<ChunkZ>>>
+        Map<String, Map<Integer, TreeSet<Integer>>> queryValuesX = new HashMap<>();
+        Map<String, Map<Integer, TreeSet<Integer>>> queryValuesZ = new HashMap<>();
+
         for (Chunk chunk : chunks) {
-            Map<Integer, TreeSet<Integer>> xMap = queryValues.get(chunk.getWorld().getName());
-            if (xMap == null) {
-                Map<Integer, TreeSet<Integer>> temp = new HashMap<>();
-                queryValues.put(chunk.getWorld().getName(), temp);
-                xMap = temp;
+            Map<Integer, TreeSet<Integer>> mapX = queryValuesX.get(chunk.getWorld().getName());
+            Map<Integer, TreeSet<Integer>> mapZ = queryValuesZ.get(chunk.getWorld().getName());
+
+            if (mapX == null) {
+                mapX = new HashMap<>();
+                queryValuesX.put(chunk.getWorld().getName(), mapX);
             }
 
-            TreeSet<Integer> zSet = xMap.get(chunk.getX());
+            if (mapZ == null) {
+                mapZ = new HashMap<>();
+                queryValuesZ.put(chunk.getWorld().getName(), mapZ);
+            }
+
+            TreeSet<Integer> zSet = mapX.get(chunk.getX());
+            TreeSet<Integer> xSet = mapZ.get(chunk.getZ());
+
             if (zSet == null) {
-                TreeSet<Integer> temp = new TreeSet<>();
-                xMap.put(chunk.getX(), temp);
-                zSet = temp;
+                zSet = new TreeSet<>();
+                mapX.put(chunk.getX(), zSet);
+            }
+
+            if (xSet == null) {
+                xSet = new TreeSet<>();
+                mapZ.put(chunk.getZ(), xSet);
             }
 
             zSet.add(chunk.getZ());
+            xSet.add(chunk.getX());
         }
+
+        String queryX = buildQuery(queryValuesX, true);
+        String queryZ = buildQuery(queryValuesZ, false);
+
+        // Pick whichever query is smaller, it's likely to have less logical branches
+        return queryX.length() < queryZ.length() ? queryX : queryZ;
+    }
+
+    private static String buildQuery(Map<String, Map<Integer, TreeSet<Integer>>> queryValues, boolean isChunkX) {
+        String chunkCoord1 = "chunk_" + (isChunkX ? "x" : "z");
+        String chunkCoord2 = "chunk_" + (isChunkX ? "z" : "x");
 
         StringBuilder queryWhere = new StringBuilder();
         for (Iterator<Map.Entry<String, Map<Integer, TreeSet<Integer>>>> iterator1 = queryValues.entrySet().iterator(); iterator1.hasNext();) {
@@ -48,11 +75,11 @@ public final class QueryUtils {
 
                 boolean isRange = values.last() - values.first() == values.size() - 1;
                 if (values.size() == 1) {
-                    queryWhere.append(String.format("chunk_x = %d AND chunk_z = %d", entry2.getKey(), values.first()));
+                    queryWhere.append(String.format("%s = %d AND %s = %d", chunkCoord1, entry2.getKey(), chunkCoord2, values.first()));
                 } else if (isRange && values.size() > 2) {
-                    queryWhere.append(String.format("chunk_x = %d AND chunk_z BETWEEN %d AND %d", entry2.getKey(), values.first(), values.last()));
+                    queryWhere.append(String.format("%s = %d AND %s BETWEEN %d AND %d", chunkCoord1, entry2.getKey(), chunkCoord2, values.first(), values.last()));
                 } else {
-                    queryWhere.append(String.format("chunk_x = %d AND chunk_z IN (", entry2.getKey()));
+                    queryWhere.append(String.format("%s = %d AND %s IN (", chunkCoord1, entry2.getKey(), chunkCoord2));
                     for (Iterator<Integer> iterator3 = entry2.getValue().iterator(); iterator3.hasNext();) {
                         Integer entry3 = iterator3.next();
                         queryWhere.append(entry3);
