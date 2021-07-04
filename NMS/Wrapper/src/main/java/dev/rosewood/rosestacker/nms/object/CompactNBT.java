4c1a3fa82574291e25957f08f37b076fc5cc3bea
Esophose <esophose@gmail.com>
2021-07-03 18:42:52 -0600

    Added documentation for new NBT classes

--- NMS/Wrapper/src/main/java/dev/rosewood/rosestacker/nms/object/CompactNBT.java
+++ NMS/Wrapper/src/main/java/dev/rosewood/rosestacker/nms/object/CompactNBT.java
@@ -8,24 +8,64 @@ import org.bukkit.entity.LivingEntity;
  */
 public interface CompactNBT {
 
+    /**
+     * Adds a new entry to the front
+     *
+     * @param entity The entity to add
+     */
     void addFirst(LivingEntity entity);
 
+    /**
+     * Adds a new entry to the end
+     *
+     * @param entity The entity to add
+     */
     void addLast(LivingEntity entity);
 
+    /**
+     * Adds all given entries to the front
+     *
+     * @param wrappedNbt The WrappedNBT entries to add
+     */
     void addAllFirst(List<WrappedNBT<?>> wrappedNbt);
 
+    /**
+     * Adds all given entries to the end
+     *
+     * @param wrappedNbt The WrappedNBT entries to add
+     */
     void addAllLast(List<WrappedNBT<?>> wrappedNbt);
 
+    /**
+     * @return A WrappedNBT object for the entity at the front of the NBT list
+     */
     WrappedNBT<?> peek();
 
+    /**
+     * Gets and removes an entity from the nbt list
+     *
+     * @return A WrappedNBT object for the entity at the front of the NBT list
+     */
     WrappedNBT<?> pop();
 
+    /**
+     * @return the number of NBT entries
+     */
     int size();
 
+    /**
+     * @return true if there are no NBT entries, false otherwise
+     */
     boolean isEmpty();
 
+    /**
+     * @return a list of all uncompressed NBT entries
+     */
     List<WrappedNBT<?>> getAll();
 
+    /**
+     * @return all compressed NBT entries serialized into a savable format
+     */
     byte[] serialize();
 
 }
--- NMS/Wrapper/src/main/java/dev/rosewood/rosestacker/nms/object/WrappedNBT.java
+++ NMS/Wrapper/src/main/java/dev/rosewood/rosestacker/nms/object/WrappedNBT.java
@@ -2,6 +2,9 @@ package dev.rosewood.rosestacker.nms.object;
 
 public interface WrappedNBT<T> {
 
+    /**
+     * @return the NMS NBT object
+     */
     T get();
 
 }
