6808ac88db55c50373c5cd6ac2b91932ac66df1f
Patrik <patrikben100hun@gmail.com>
2021-08-30 21:00:37 +0200

    Update HungarianLocale.java

--- Plugin/src/main/java/dev/rosewood/rosestacker/locale/HungarianLocale.java
+++ Plugin/src/main/java/dev/rosewood/rosestacker/locale/HungarianLocale.java
@@ -9,7 +9,7 @@ import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-public class EnglishLocale implements Locale {
+public class HungarianLocale implements Locale {
 
     @Override
     public String getLocaleName() {
