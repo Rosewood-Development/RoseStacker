511dc8fa87f5b292cb5e8bdf7344e26fce1e72e9
Esophose <esophose@gmail.com>
2022-02-21 00:02:39 -0700

    Fix breeding stacks giving significantly too much experience

--- Plugin/src/main/java/dev/rosewood/rosestacker/listener/BreedingListener.java
+++ Plugin/src/main/java/dev/rosewood/rosestacker/listener/BreedingListener.java
@@ -96,7 +96,7 @@ public class BreedingListener implements Listener {
                         PersistentDataUtils.removeEntityAi(baby);
                 });
 
-            StackerUtils.dropExperience(animal.getLocation(), stackSize, 7 * stackSize, stackSize);
+            StackerUtils.dropExperience(animal.getLocation(), totalChildren, 7 * totalChildren, totalChildren);
         }, 30);
     }
 
