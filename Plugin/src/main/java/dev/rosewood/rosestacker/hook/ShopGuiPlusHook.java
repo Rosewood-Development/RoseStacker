e9b159d3c254828d5fc96d092dc2259f54e91a36
Esophose <esophose@gmail.com>
2023-03-26 13:16:39 -0600

    Fix ShopGUIPlus dependency

--- Plugin/src/main/java/dev/rosewood/rosestacker/hook/ShopGuiPlusHook.java
+++ Plugin/src/main/java/dev/rosewood/rosestacker/hook/ShopGuiPlusHook.java
@@ -3,6 +3,7 @@ package dev.rosewood.rosestacker.hook;
 import dev.rosewood.rosegarden.RosePlugin;
 import dev.rosewood.rosestacker.manager.StackManager;
 import net.brcdev.shopgui.ShopGuiPlusApi;
+import net.brcdev.shopgui.exception.api.ExternalSpawnerProviderNameConflictException;
 
 public final class ShopGuiPlusHook {
 
@@ -12,8 +13,13 @@ public final class ShopGuiPlusHook {
      * @param rosePlugin The plugin instance
      */
     public static void setupSpawners(RosePlugin rosePlugin) {
-        if (rosePlugin.getManager(StackManager.class).isSpawnerStackingEnabled())
-            ShopGuiPlusApi.registerSpawnerProvider(new RoseStackerSpawnerProvider(rosePlugin));
+        if (rosePlugin.getManager(StackManager.class).isSpawnerStackingEnabled()) {
+            try {
+                ShopGuiPlusApi.registerSpawnerProvider(new RoseStackerSpawnerProvider(rosePlugin));
+            } catch (ExternalSpawnerProviderNameConflictException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 }
